87a1f9cf3740dc37d7461ecd0e00d9c3761368d0
Panov Yury <u.panow@yandex.ru>
2022-10-11 23:56:45 +0500

    Генерация массива объектов изображений

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ const getRandomPositiveNumber = (start, end) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
 /**
  * Функция проверяет допустимость длины строки
  * @param {String} text проверяемая строка
@@ -37,5 +36,52 @@ const getRandomPositiveNumber = (start, end) => {
  */
 const validateTextLength = (text, length) => text.length <= length;
 
-getRandomPositiveNumber(1, 5);
+// Набор описаний изображений для генерации данных
+// Источник: https://rusability.ru/pfanshtil/200-fraz-podpisei-k-foto-i-selfi-v-sotssetyah/5fd295882dda593c3483dffb
+const descriptions = [
+  'Открываю для себя мир. Скоро вернусь',
+  'Если я не найду выход, я его создам',
+  'Если не сейчас, то когда? Если не я, то кто?',
+  'Успех – встреча возможности и готовности действовать',
+  'Чемпионы играют до тех пор, пока не получат желаемое',
+  'Какая страна покорила ваше сердце?',
+  'Лучшие моменты ждут вас за пределами зоны комфорта',
+  'Нельзя купить счастье, но можно купить билеты на самолет',
+  'Просто живу так, как считаю нужным',
+  'Да, еще одно фото'
+];
+
+/**
+ * Функция для получения случайного элемента из массива
+ * @param {Array} array массив с данными
+ * @returns случайны элемент
+ */
+const getRandomArrayElement = (array) => {
+  const index = getRandomPositiveNumber(0, array.length);
+  return array[index];
+};
+
+/**
+ * Функция генерирует объект изображения со случайными данными
+ * @param {Number} index идентификатор объекта
+ * @returns объект изображения
+ */
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(descriptions),
+  likes: getRandomPositiveNumber(15, 200),
+  comments: getRandomPositiveNumber(0, 200),
+});
+
+const PICTURES_NUMBER = 25;
+
+/**
+ * Функция генерирует массив случайных изображений
+ * @returns массив объектов изображений
+ */
+const getRandomPictures = () =>
+  Array.from({length: PICTURES_NUMBER}, (_, index) => createPicture(index + 1));
+
 validateTextLength('Hello, world!', 10);
+getRandomPictures();
